f79cab603500a675c4074942841826da717d676c
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-24 13:31:07 -0500

    Completing Stripe Payment

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,4 +1,14 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
 exports.stripe = async (req, res) => {
-    console.log(req.body)
-    res.send('Stripe Route')
+    const {purchase, total_amount, shipping_fee} = req.body;
+    const calculateOrderAmount = () => {
+        return total_amount + shipping_fee;
+    };
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'usd',
+    });
+    console.log(paymentIntent);
+    res.json({ clientSecret: paymentIntent.client_secret});
 };
